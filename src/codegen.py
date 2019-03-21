b057897ecff6e48b23bf8e7c65bafb850ce082a0
Christopher Frederickson <chris@insituware.com>
2019-03-21 19:41:29 -0400

    Edit generator to more closely follow style guidelines

--- src/codegen.py
+++ src/codegen.py
@@ -14,97 +14,117 @@
 import getopt, os, sys
 import click
 from yaml import load, dump
+
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
 from jinja2 import Environment, FileSystemLoader
 
-_VERSION = '0.1.0'
+_VERSION = "0.1.0"
 _DEBUG = False
 
-def generateSourceFile(template, peripheral, templateExtension, outputDir):
-  """
-  Generates a source file for a provided Jinja2 template.
-  """
-  # Open peripheral file
-  with open(peripheral, 'r') as peripheralFile:
-    peripheralData = load(peripheralFile, Loader=Loader)
-    # Add additional metadata to the spec data
-    peripheralData['version'] = _VERSION
-    peripheralData['fileName'] = peripheral
 
-    if _DEBUG:
-      print(peripheralData)
-    
-    # Render file
-    peripheralGen = template.render(peripheralData)
-    # Get file path
-    # Take into consideration the package
-    if _DEBUG:
-      print(peripheralData['info']['title'])
-    packagePath = peripheralData['info']['package'].replace('.', '/')
-    outputFilePath = os.path.join(outputDir, os.path.normpath(packagePath))
-    if not os.path.exists(outputFilePath):
-      try:
-        os.makedirs(outputFilePath)
-      except:
-        print('Could not make file directory', outputFilePath)
-        sys.exit(1)
+def generate_source_file(template, peripheral, template_extension, output_dir):
+    """
+    Generates a source file for a provided Jinja2 template.
 
-    peripheralOutputPath = os.path.join(
-      outputFilePath, 
-      peripheralData['info']['title'] + templateExtension
-    )
-    with open(peripheralOutputPath, 'x') as peripheralOutputFile:
-      peripheralOutputFile.write(peripheralGen)
-
-def generateSourceFilesForTemplate(env, templateFile, inputFiles, outputDir):
+    Args:
+        template: A single file that is part of the template.
+        peripheral: A single CyanoByte document to generate.
+        template_extension: The file extension of the output.
+        output_dir: The directory to output the generated files.
   """
-  Generates a series of source files for a provided template file.
-  """
-  # Open template
-  with open(templateFile, 'r') as templateContents:
-    templateObject = env.from_string(
-      templateContents.read()
-    )
-    _, templateExtension = os.path.splitext(templateFile)
-    
-    # Create output dir
-    if not os.path.exists(outputDir):
-      try:
-        os.makedirs(outputDir)
-      except:
-        print('Could not make output directory', outputDir)
-        sys.exit(1)
+    # Open peripheral file
+    with open(peripheral, "r") as peripheral_file:
+        peripheral_data = load(peripheral_file, Loader=Loader)
+        # Add additional metadata to the spec data
+        peripheral_data["version"] = _VERSION
+        peripheral_data["fileName"] = peripheral
 
-    for peripheral in inputFiles:
-      generateSourceFile(templateObject, peripheral, templateExtension, outputDir)
+        if _DEBUG:
+            print(peripheral_data)
+
+        # Render file
+        peripheral_gen = template.render(peripheral_data)
+        # Get file path
+        # Take into consideration the package
+        if _DEBUG:
+            print(peripheral_data["info"]["title"])
+        package_path = peripheral_data["info"]["package"].replace(".", "/")
+        output_file_path = os.path.join(output_dir, os.path.normpath(package_path))
+        if not os.path.exists(output_file_path):
+            try:
+                os.makedirs(output_file_path)
+            except:
+                print("Could not make file directory", output_file_path)
+                sys.exit(1)
+
+        peripheral_output_path = os.path.join(
+            output_file_path, peripheral_data["info"]["title"] + template_extension
+        )
+        with open(peripheral_output_path, "x") as peripheral_output_file:
+            peripheral_output_file.write(peripheral_gen)
+
+
+def generate_source_files_for_template(env, template_file, input_files, output_dir):
+    """
+    Generates a series of source files for a provided template file.
+
+    Args:
+        env: Jinja2 environment used during generation.
+        template_file: A single file that is part of the template.
+        input_files: A list of CyanoByte documents to generate.
+        output_dir: The directory to output the generated files.
+    """
+    # Open template
+    with open(template_file, "r") as template_contents:
+        template_object = env.from_string(template_contents.read())
+        _, template_extension = os.path.splitext(template_file)
+
+        # Create output dir
+        if not os.path.exists(output_dir):
+            try:
+                os.makedirs(output_dir)
+            except:
+                print("Could not make output directory", output_dir)
+                sys.exit(1)
+
+        for peripheral in input_files:
+            generate_source_file(
+                template_object, peripheral, template_extension, output_dir
+            )
+
 
 @click.command()
-@click.option('-i', '--input', 'inputFiles', multiple=True)
-@click.option('-t', '--template', 'templateFiles', multiple=True)
-@click.option('-o', '--output', 'outputDir', default='./build', show_default=True)
-@click.option('-d', '--debug', 'debug', default=False)
-def gen(inputFiles, templateFiles, outputDir, debug):
-  """
-  Takes command line arguments and generates source files for every peripheral
-  to each template file.
-  """
-  global _DEBUG
-  _DEBUG = debug
+@click.option("-i", "--input", "input_files", multiple=True)
+@click.option("-t", "--template", "template_files", multiple=True)
+@click.option("-o", "--output", "output_dir", default="./build", show_default=True)
+@click.option("-d", "--debug", "debug", default=False)
+def gen(input_files, template_files, output_dir, debug):
+    """
+    Takes command line arguments and generates source files for every peripheral
+    to each template file.
 
-  if _DEBUG:
-    print("Generating " + str(len(inputFiles)) + " file(s)")
+    Args:
+        input_files: A list of CyanoByte documents to generate.
+        template_files: A list of files that are part of the template.
+        output_dir: The directory to output the generated files.
+        debug: Print debug messages?
+    """
+    global _DEBUG
+    _DEBUG = debug
 
-  # Setup Jinja environment
-  env = Environment(
-    loader=FileSystemLoader('./templates'),
-    trim_blocks=True,
-    lstrip_blocks=True
-  )
-  for templateFile in templateFiles:
-    generateSourceFilesForTemplate(env, templateFile, inputFiles, outputDir)
+    if _DEBUG:
+        print("Generating " + str(len(input_files)) + " file(s)")
+
+    # Setup Jinja2 environment
+    env = Environment(
+        loader=FileSystemLoader("./templates"), trim_blocks=True, lstrip_blocks=True
+    )
+    for template_file in template_files:
+        generate_source_files_for_template(env, template_file, input_files, output_dir)
+
 
 if __name__ == "__main__":
     gen(sys.argv[1:])
