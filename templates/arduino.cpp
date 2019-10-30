8fac6325f0efec9452fd22708d87b802fdb49446
Nick Felker <fleker@google.com>
2019-10-30 17:04:50 -0400

    Move logical ops for registers and functions to inline
    Add C utils helper
    
    Addresses comments in #99

--- templates/arduino.cpp
+++ templates/arduino.cpp
@@ -1,4 +1,5 @@
 {% import 'macros.jinja2' as utils %}
+{% import 'ctils.jinja2' as ctils %}
 {% set template = namespace(enum=false, sign=false, math=false, struct=false) %}
 /*
 {{ utils.pad_string('* ', utils.license(info.copyright.name, info.copyright.date, info.license.name)) -}}
@@ -8,198 +9,43 @@
 * Class for {{ info.title }}
 {{utils.pad_string("* ", info.description)}}
 */
-{% macro params_int(input) -%}
-{% for variable in input %}
-{% for key in variable.keys() -%}
-{% if key != 'value' %}{{key}}, {% endif %}
-{%- endfor %}
-{% endfor %}
-{%- endmacro %}
-
-{% macro params(input) -%}
-{{params_int(input)[:-2]}}
-{%- endmacro %}
-
-{% macro variables(vars) -%}
-{% for var in vars %}
-{% for key in var.keys() %}
-    {{ numconv(var[key]) }} {{key}}; // Variable declaration
-{% endfor %}
-{% endfor %}
-{%- endmacro %}
-
-{% macro numconv(cyan) -%}
-{%- if cyan == 'int8' -%}
-char
-{%- elif cyan == 'uint8' -%}
-uint8_t
-{%- elif cyan == 'int16' -%}
-short
-{%- elif cyan == 'uint16' -%}
-uint16_t
-{%- elif cyan == 'float32' -%}
-float
-{%- else -%}
-/* ERROR UNDEFINED TYPE */
-{%- endif -%}
-{%- endmacro %}
-
-{% macro numtype(len, sign) -%}
-{% if sign %}
-{% if len <= 8 %}
-char
-{% elif len <= 16 %}
-short
-{% elif len <= 32 %}
-long
-{% else %}
-long long
-{% endif %}
-{% else %}
-{% if len <= 8 %}
-uint8_t
-{% elif len <= 16 %}
-uint16_t
-{% elif len <= 32 %}
-uint32_t
-{% else %}
-uint64_t
-{% endif %}
-{% endif %}
-{%- endmacro %}
 
 {% macro logic(logicalSteps, function) -%}
 
-{% for step in logicalSteps %}
-{% for key in step.keys() %}
+{% for step in logicalSteps -%}
+{% for key in step.keys() -%}
 {# Check if assignment is a send-op #}
 {% if key == 'cmdWrite' %}
     write{{step.register[12:]}}({{step.value}});
     {% break %}
-{% endif %}
+{%- endif %}
+
 {# Check if assignment op #}
-{% if step[key][0:1] == "=" %}
+{% if step[key][:1] == "=" %}
     {{key}} {{step[key]}}
-{% endif %}
+{%- endif %}
 
 {# Check if assignment is a send-op #}
 {% if key == 'send' %}
     write{{function.register[12:]}}({{step[key]}});
-{% endif %}
+{%- endif %}
+
+{# Check if assignment is register read op #}
+{% if step[key][:12] == '#/registers/' %}
+    {{key}} = read{{step[key][12:]}}();
+{%- endif %}
+
+{# Check if assignment is function call op #}
+{% if step[key][:12] == '#/functions/' %}
+    {{key}} = {{step[key] | regex_replace('#/functions/(?P<function>.+)/(?P<compute>.+)', '\\g<function>\\g<compute>')}}();
+{%- endif %}
 
 {# If the value is a list, then this is a logical setter #}
 {% if step[key] is iterable and step[key] is not string %}
-    {{key}} = {{recursiveAssignLogic(step[key][0], step[key][0].keys()) -}};
-{% endif %}
+    {{key}} = {{ ctils.recursiveAssignLogic(step[key][0], step[key][0].keys()) -}};
+{%- endif %}
 
-{% endfor %}
-{% endfor %}
-{%- endmacro %}
-
-{% macro recursiveAssignLogic(logicalStep, keys) -%}
-{% for key in keys -%}
-{# Read from a register #}
-{# {{key}} {{logicalStep[key]}} #}
-{% if key == 'read' -%}
-    read{{logicalStep[key][12:]}}()
-{%- endif %}
-{# Read from a function #}
-{# {{key}} {{logicalStep[key]}} #}
-{% if key == 'function' -%}
-    {{logicalStep[key] | regex_replace('#/functions/(?P<function>.+)/(?P<compute>.+)', '\\g<function>_\\g<compute>')}}()
-{%- endif %}
-{# Perform a recursive summation from an array of logical steps #}
-{% if key == 'sum' -%}
-    ({%- for step in logicalStep[key] -%}
-    {% if step is iterable and step is not string -%}
-    {{ recursiveAssignLogic(step, step.keys()) -}}
-    {%- else -%}
-    {{step}}
-    {%- endif %}
-    {{- "+" if not loop.last -}}
-    {%- endfor -%})
-{%- endif %}
-{# Perform a recursive difference from an array of logical steps #}
-{% if key == 'difference' -%}
-    ({%- for step in logicalStep[key] -%}
-    {% if step is iterable and step is not string -%}
-    {{ recursiveAssignLogic(step, step.keys()) -}}
-    {%- else -%}
-    {{step}}
-    {%- endif %}
-    {{- "-" if not loop.last -}}
-    {%- endfor -%})
-{%- endif %}
-{# Perform a recursive product from an array of logical steps #}
-{% if key == 'product' -%}
-    ({%- for step in logicalStep[key] -%}
-    {% if step is iterable and step is not string -%}
-    {{ recursiveAssignLogic(step, step.keys()) -}}
-    {%- else -%}
-    {{step}}
-    {%- endif %}
-    {{- "*" if not loop.last -}}
-    {%- endfor -%})
-{%- endif %}
-{# Perform a recursive division from an array of logical steps #}
-{% if key == 'division' -%}
-    ({%- for step in logicalStep[key] -%}
-    {% if step is iterable and step is not string -%}
-    {{ recursiveAssignLogic(step, step.keys()) -}}
-    {%- else -%}
-    {{ step }}
-    {%- endif %}
-    {{- "/" if not loop.last -}}
-    {%- endfor -%})
-{%- endif %}
-{# Perform a recursive modulus from an array of logical steps #}
-{% if key == 'modulus' -%}
-    ({%- for step in logicalStep[key] -%}
-    {% if step is iterable and step is not string -%}
-    {{ recursiveAssignLogic(step, step.keys()) -}}
-    {%- else -%}
-    {{ step }}
-    {%- endif %}
-    {{- "%" if not loop.last -}}
-    {%- endfor -%})
-{%- endif %}
-{# Perform a bitwise OR from an array of logical steps #}
-{% if key == 'bitwiseOr' -%}
-    ({%- for step in logicalStep[key] -%}
-    {% if step is iterable and step is not string -%}
-    {{ recursiveAssignLogic(step, step.keys()) -}}
-    {%- else -%}
-    {{step}}
-    {%- endif %}
-    {{- "|" if not loop.last -}}
-    {%- endfor -%})
-{%- endif %}
-{# Perform a bitwise AND from an array of logical steps #}
-{% if key == 'bitwiseAnd' -%}
-    ({%- for step in logicalStep[key] -%}
-    {% if step is iterable and step is not string -%}
-    {{ recursiveAssignLogic(step, step.keys()) -}}
-    {%- else -%}
-    {{step}}
-    {%- endif %}
-    {{- "&" if not loop.last -}}
-    {%- endfor -%})
-{%- endif %}
-{# Perform a power operation #}
-{% if key == 'power' -%}
-    pow({{logicalStep[key][0]}}, {{logicalStep[key][1]}})
-{%- endif %}
-{# Perform an arc tangent operation #}
-{% if key == 'arc tangent' -%}
-    atan({{logicalStep[key]}})
-{%- endif %}
-{# Bitwise ops #}
-{%- if key == 'bitShiftLeft' -%}
-    ({{logicalStep.var}} << {{logicalStep.bits}})
-{%- endif %}
-{%- if key == 'bitShiftRight' -%}
-    ({{logicalStep.var}} >> {{logicalStep.bits}})
-{%- endif %}
+{%- endfor %}
 {%- endfor %}
 {%- endmacro %}
 
@@ -270,30 +116,38 @@ void {{info.title}}::end() {
 
 {% for register in registers -%}
 {% for key in register.keys() %}
-int {{info.title}}::read{{key}}() {
-    uint8_t value;
-    {% set length = (register[key].length / 8) | round(1, 'ceil') %}
+{% set length = register[key].length %}
+{% set bytes = (register[key].length / 8) | round(1, 'ceil') | int %}
+{{ctils.numtype(length)}} {{info.title}}::read{{key}}() {
+    uint8_t datum;
+    {{ctils.numtype(length)}} value;
     _wire->beginTransmission(DEVICE_ADDRESS);
     _wire->write(REGISTER_{{key.upper()}});
     if (_wire->endTransmission(false) != 0) {
         return -1;
     }
 
-    if (_wire->requestFrom(DEVICE_ADDRESS, {{length}}) != {{length}}) {
+    if (_wire->requestFrom(DEVICE_ADDRESS, {{bytes}}) != {{bytes}}) {
         return 0;
     }
 
-    for (size_t i = 0; i < {{length}}; i++) {
-        *value++ = wire->read();
-    }
+    {# Read a byte at a time #}
+    {% for n in range(bytes) %}
+    datum = wire->read();
+    value = value << 8 | datum;
+    {% endfor %}
 
     return value;
 }
 
-int {{info.title}}::write{{key}}(uint8_t data) {
+int {{info.title}}::write{{key}}({{ctils.numtype(length)}} data) {
     _wire->beginTransmission(DEVICE_ADDRESS);
-    _wire->write(REGISTER_{{key.upper()}});
-    _wire->write(data);
+    // Put our data into uint8_t buffer
+    uint8_t buffer[{{bytes + 1}}] = { (uint8_t) REGISTER_{{key.upper()}} };
+    {% for n in range(bytes) %}
+    uint8_t buffer[{{n + 1}}] = (data >> {{8 * (bytes - n - 1)}}) & 0xFF;
+    {% endfor %}
+    _wire->write(buffer, {{bytes + 1}});
     if (_wire->endTransmission() != 0) {
         return 0;
     }
@@ -307,7 +161,7 @@ int {{info.title}}::write{{key}}(uint8_t data) {
 {% for key in function.keys() %}
 {% if 'R' is in(function[key].readWrite) %}
 {# Getter #}
-{{numtype(register[function[key].register[12:]].length)}} {{info.title}}::get{{key}}() {
+{{ctils.numtype(register[function[key].register[12:]].length)}} {{info.title}}::get{{key}}() {
     // Read register data
     // '#/registers/{{function[key].register[12:]}}' > '{{function[key].register[12:]}}'
     uint8_t val = read{{function[key].register[12:]}}()
@@ -324,14 +178,14 @@ int {{info.title}}::write{{key}}(uint8_t data) {
 {%- if 'W' is in(function[key].readWrite) %}
 {# Setter #}
 
-{{numtype(register[function[key].register[12:]].length)}} {{info.title}}::set{{key}}(uint8_t data) {
+{{ctils.numtype(register[function[key].register[12:]].length)}} {{info.title}}::set{{key}}(uint8_t data) {
     {% if function[key].bitEnd %}
     // Bitshift value
     data = data << {{function[key].bitEnd}}
     {% endif %}
     // Read current register data
     // '#/registers/{{function[key].register[12:]}}' > '{{function[key].register[12:]}}'
-    uint8_t register_data = read{{function[key].register[12:].lower()}}()
+    {{ctils.numtype(register[function[key].register[12:]].length)}} register_data = read{{function[key].register[12:].lower()}}()
     register_data = register_data | data
     return write{{function[key].register[12:]}}(register_data)
 }
@@ -341,12 +195,12 @@ int {{info.title}}::write{{key}}(uint8_t data) {
 {% for compute in function[key].computed %}
 {% for computeKey in compute.keys() %}
 {% if compute[computeKey].input %}
-{{numconv(compute[computeKey].returnType)}} {{info.title}}::{{key}}{{computeKey}}({{params(compute[computeKey].input)}}) {
+{{ctils.numconv(compute[computeKey].returnType)}} {{info.title}}::{{key}}{{computeKey}}({{ctils.params(compute[computeKey].input)}}) {
 {% else %}
-{{numconv(compute[computeKey].returnType)}} {{info.title}}::{{key}}{{computeKey}}() {
+{{ctils.numconv(compute[computeKey].returnType)}} {{info.title}}::{{key}}{{computeKey}}() {
 {% endif %}
     {# Declare our variables #}
-{{ variables(compute[computeKey].variables) }}
+{{ ctils.variables(compute[computeKey].variables) }}
 
     {# Read `value` if applicable #}
     {%- for variable in compute[computeKey].input %}
--- templates/arduino.h
+++ templates/arduino.h
@@ -1,4 +1,5 @@
 {% import 'macros.jinja2' as utils %}
+{% import 'ctils.jinja2' as ctils %}
 {% set template = namespace(math=false) %}
 /*
 {{ utils.pad_string('* ', utils.license(info.copyright.name, info.copyright.date, info.license.name)) -}}
@@ -8,46 +9,6 @@
 * Class for {{ info.title }}
 {{utils.pad_string("* ", info.description)}}
 */
-
-{% macro numconv(cyan) -%}
-{%- if cyan == 'int8' -%}
-char
-{%- elif cyan == 'uint8' -%}
-uint8_t
-{%- elif cyan == 'int16' -%}
-short
-{%- elif cyan == 'uint16' -%}
-uint16_t
-{%- elif cyan == 'float32' -%}
-float
-{%- else -%}
-/* ERROR UNDEFINED TYPE */
-{%- endif -%}
-{%- endmacro %}
-
-{% macro numtype(len, sign) -%}
-{%- if sign -%}
-{%- if len <= 8 -%}
-char
-{%- elif len <= 16 -%}
-short
-{%- elif len <= 32 -%}
-long
-{%- else -%}
-long long
-{%- endif -%}
-{%- else -%}
-{%- if len <= 8 -%}
-uint8_t
-{%- elif len <= 16 -%}
-uint16_t
-{%- elif len <= 32 -%}
-uint32_t
-{%- else -%}
-uint64_t
-{%- endif -%}
-{%- endif -%}
-{%- endmacro %}
 
 #ifndef _{{info.title}}_H_
 #define _{{info.title}}_H_
@@ -94,7 +55,7 @@ class {{info.title}} {
         /**
 {{utils.pad_string("         * ", register[key].description)}}
          */
-        {{numtype(register[key].length)}} read{{key}}();
+        {{ctils.numtype(register[key].length)}} read{{key}}();
 
         /**
 {{utils.pad_string("         * ", register[key].description)}}
@@ -108,7 +69,7 @@ class {{info.title}} {
         /**
 {{utils.pad_string("         * ", function[key].description)}}
          */
-        {{numtype(register[function[key].register[12:]].length)}} get{{key}}();
+        {{ctils.numtype(register[function[key].register[12:]].length)}} get{{key}}();
         {% endif %}
         {% if 'W' is in(function[key].readWrite) %}
         /**
@@ -124,9 +85,9 @@ class {{info.title}} {
 {{utils.pad_string("         * ", function[key].description)}}
          */
         {% if compute[computeKey].input %}
-        {{numconv(compute[computeKey].returnType)}} {{key}}{{computeKey}}({{params(compute[computeKey].input)}});
+        {{ctils.numconv(compute[computeKey].returnType)}} {{key}}{{computeKey}}({{ctils.params(compute[computeKey].input)}});
         {% else %}
-        {{numconv(compute[computeKey].returnType)}} {{key}}{{computeKey}}();
+        {{ctils.numconv(compute[computeKey].returnType)}} {{key}}{{computeKey}}();
         {% endif %}
         {% endfor %}
         {% endfor %}
