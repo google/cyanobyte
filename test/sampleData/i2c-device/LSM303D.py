ba79ec44b7cb87e9617c7996fc077b8d74b3edd7
Nick <handnf@gmail.com>
2020-04-11 16:00:10 -0400

    Add i2c-device as a device template
    Fixes #170

---
+++ test/sampleData/i2c-device/LSM303D.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Auto-generated file for LSM303D v0.1.0.
+# Generated from peripherals/LSM303D.yaml using Cyanobyte Codegen v0.1.0
+from i2cdevice import Device, Register, BitField
+
+I2C_ADDR = 29
+
+ACCELEROMETERX_HIGH = Register('ACCELEROMETERX_HIGH', 169, read_only=False, bitwidth=8)
+ACCELEROMETERX_LOW = Register('ACCELEROMETERX_LOW', 168, read_only=False, bitwidth=8)
+ACCELEROMETERY_HIGH = Register('ACCELEROMETERY_HIGH', 171, read_only=False, bitwidth=8)
+ACCELEROMETERY_LOW = Register('ACCELEROMETERY_LOW', 170, read_only=False, bitwidth=8)
+ACCELEROMETERZ_HIGH = Register('ACCELEROMETERZ_HIGH', 173, read_only=False, bitwidth=8)
+ACCELEROMETERZ_LOW = Register('ACCELEROMETERZ_LOW', 172, read_only=False, bitwidth=8)
+MAGNETOMETERX_HIGH = Register('MAGNETOMETERX_HIGH', 137, read_only=False, bitwidth=8)
+MAGNETOMETERX_LOW = Register('MAGNETOMETERX_LOW', 136, read_only=False, bitwidth=8)
+MAGNETOMETERY_HIGH = Register('MAGNETOMETERY_HIGH', 139, read_only=False, bitwidth=8)
+MAGNETOMETERY_LOW = Register('MAGNETOMETERY_LOW', 138, read_only=False, bitwidth=8)
+MAGNETOMETERZ_HIGH = Register('MAGNETOMETERZ_HIGH', 141, read_only=False, bitwidth=8)
+MAGNETOMETERZ_LOW = Register('MAGNETOMETERZ_LOW', 140, read_only=False, bitwidth=8)
+
+lsm303d = Device(I2C_ADDR, registers=(
+    ACCELEROMETERX_HIGH,
+    ACCELEROMETERX_LOW,
+    ACCELEROMETERY_HIGH,
+    ACCELEROMETERY_LOW,
+    ACCELEROMETERZ_HIGH,
+    ACCELEROMETERZ_LOW,
+    MAGNETOMETERX_HIGH,
+    MAGNETOMETERX_LOW,
+    MAGNETOMETERY_HIGH,
+    MAGNETOMETERY_LOW,
+    MAGNETOMETERZ_HIGH,
+    MAGNETOMETERZ_LOW
+))
---
+++ test/sampleData/i2c-device/MCP9808.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Auto-generated file for MCP9808 v0.1.0.
+# Generated from peripherals/MCP9808.yaml using Cyanobyte Codegen v0.1.0
+from i2cdevice import Device, Register, BitField
+
+I2C_ADDR_24 = 24
+I2C_ADDR_25 = 25
+I2C_ADDR_26 = 26
+I2C_ADDR_27 = 27
+I2C_ADDR_28 = 28
+I2C_ADDR_29 = 29
+I2C_ADDR_30 = 30
+I2C_ADDR_31 = 31
+I2C_ADDR = [I2C_ADDR_24, I2C_ADDR_25, I2C_ADDR_26, I2C_ADDR_27, I2C_ADDR_28, I2C_ADDR_29, I2C_ADDR_30, I2C_ADDR_31]
+
+CONFIGURATION = Register('CONFIGURATION', 1, fields=(
+    BitField('limitHysteresis', 0b0000001100000000, bitwidth=2, values_map={
+        Temp_0C: 0,
+        Temp_1C5: 1,
+        Temp_3C: 2,
+        Temp_6C: 3
+    })
+    BitField('shutdownMode', 0b0000000010000000, bitwidth=1, values_map={
+        continousConversion: 0,
+        shutdown: 1
+    })
+), read_only=False, bitwidth=16)
+
+mcp9808 = Device(I2C_ADDR, registers=(
+    CONFIGURATION
+))
